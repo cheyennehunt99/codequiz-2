e0fb74967baaa726cf1086209887feca7503ba6c
Chien81 <cheyennehunt78@gmail.com>
2020-07-10 14:18:25 -0700

    updating js timer

--- assets/script.js
+++ assets/script.js
@@ -81,6 +81,12 @@ function showScores() {
 var questions = [
     new Question("What's 9+10?", ["21", "9","35", "19"], "21"),
     new Question("What is my favorite color?", ["Porpoise", "Turquoise", "Maroon", "Lavender"], "Maroon"),
+    new Question("What is the seventh planet from the sun?", ["Jupiter", "Saturn", "Uranus", "Neptune"], "Uranus"),
+    new Question("", ["", "","", ""], ""),
+    new Question("", ["", "","", ""], ""),
+    new Question("", ["", "","", ""], ""),
+    new Question("", ["", "","", ""], ""),
+   
 ];
  
 // create quiz
@@ -89,3 +95,37 @@ var quiz = new Quiz(questions);
 // display quiz
 populate();
 
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 5);
+    
+    return {
+      total,
+      minutes,
+      seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+  
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  initializeClock('clockdiv', deadline);
